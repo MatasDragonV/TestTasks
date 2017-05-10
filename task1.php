1332f0ec4b53fbe512d98202ee4640e13b511bbe
Oleg Matasov <admin@2mhost.ru>
2017-05-11 01:56:55 +0300

    Task1 completed

---
+++ task1.php
@@ -0,0 +1,21 @@
+<?php
+//Самый простой способ отфильтровать инфоблок по внешнему полю - сделать запрос по значению связанного поля. Например так.
+$PRODUCTS_IBLOCK_ID = 20;
+$Producer = 'Samsung';
+$arFilter = array(
+    "IBLOCK_ID" => $PRODUCTS_IBLOCK_ID,
+    "ACTIVE" => "Y",
+    array(
+        "LOGIC" => "OR",
+        array(">=TIMESTAMP_X" => ConvertTimeStamp(time()-(86400*7)),"<=TIMESTAMP_X" => ConvertTimeStamp(time())),
+        array("PROPERTY_PRODUCER.NAME" => $Producer),
+    ),
+);
+$arSelect = Array("*");//Выбираем все поля
+$res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+while($ob = $res->GetNextElement())
+{
+ $arFields = $ob->GetFields();
+ print_r($arFields);
+}
+?>
