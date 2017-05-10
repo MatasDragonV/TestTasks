ce29264b17ede66f83c8c5ec26e1b30dbe3627ab
Oleg Matasov <admin@2mhost.ru>
2017-05-11 02:38:23 +0300

    Added module check & Prolog

--- task1.php
+++ task1.php
@@ -1,21 +1,25 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
+if(CModule::IncludeModule("iblock"))
+{ 
 //Самый простой способ отфильтровать инфоблок по внешнему полю - сделать запрос по значению связанного поля. Например так.
-$PRODUCTS_IBLOCK_ID = 20;
-$Producer = 'Samsung';
-$arFilter = array(
-    "IBLOCK_ID" => $PRODUCTS_IBLOCK_ID,
-    "ACTIVE" => "Y",
-    array(
-        "LOGIC" => "OR",
-        array(">=TIMESTAMP_X" => ConvertTimeStamp(time()-(86400*7)),"<=TIMESTAMP_X" => ConvertTimeStamp(time())),
-        array("PROPERTY_PRODUCER.NAME" => $Producer),
-    ),
-);
-$arSelect = Array("*");//Выбираем все поля
-$res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
-while($ob = $res->GetNextElement())
-{
- $arFields = $ob->GetFields();
- print_r($arFields);
+    $PRODUCTS_IBLOCK_ID = 20;
+    $Producer = 'Samsung';
+    $arFilter = array(
+        "IBLOCK_ID" => $PRODUCTS_IBLOCK_ID,
+        "ACTIVE" => "Y",
+        array(
+            "LOGIC" => "OR",
+            array(">=TIMESTAMP_X" => ConvertTimeStamp(time()-(86400*7)),"<=TIMESTAMP_X" => ConvertTimeStamp(time())),
+            array("PROPERTY_PRODUCER.NAME" => $Producer),
+        ),
+    );
+    $arSelect = Array("*");//Выбираем все поля
+    $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+    while($ob = $res->GetNextElement())
+    {
+        $arFields = $ob->GetFields();
+        print_r($arFields);
+    }
 }
 ?>
