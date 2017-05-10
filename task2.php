9ae686b7466ffcb8cf7cb465031e46ee58116979
Oleg Matasov <admin@2mhost.ru>
2017-05-11 02:40:21 +0300

    Added prolog & module check

--- task2.php
+++ task2.php
@@ -1,14 +1,18 @@
 <?php
-$arFilter = Array(
-	">=DATE_INSERT" => ConvertTimeStamp(time()-(86400*7)),
-	"<=DATE_INSERT" => ConvertTimeStamp(time()),
-	">DISCOUNT_VALUE" => 0,
-	"USER_PHONE" => "7%"
-   );
+require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
+if(CModule::IncludeModule("iblock"))
+{ 
+	$arFilter = Array(
+		">=DATE_INSERT" => ConvertTimeStamp(time()-(86400*7)),
+		"<=DATE_INSERT" => ConvertTimeStamp(time()),
+		">DISCOUNT_VALUE" => 0,
+		"USER_PHONE" => "7%"
+   	);
 
-$db_sales = CSaleOrder::GetList(array("DATE_INSERT" => "ASC"), $arFilter);
-while ($ar_sales = $db_sales->Fetch())
-{
-   print_r($db_sales->Fetch());
+	$db_sales = CSaleOrder::GetList(array("DATE_INSERT" => "ASC"), $arFilter);
+	while ($ar_sales = $db_sales->Fetch())
+	{
+		print_r($db_sales->Fetch());
+	}
 }
 ?>
