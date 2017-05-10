012417de7bb69737966f9c037a40c63718e903cb
Oleg Matasov <admin@2mhost.ru>
2017-05-11 02:14:15 +0300

    Task 2 Completed

---
+++ task2.php
@@ -0,0 +1,14 @@
+<?php
+$arFilter = Array(
+	">=DATE_INSERT" => ConvertTimeStamp(time()-(86400*7)),
+	"<=DATE_INSERT" => ConvertTimeStamp(time()),
+	">DISCOUNT_VALUE" => 0,
+	"USER_PHONE" => "7%"
+   );
+
+$db_sales = CSaleOrder::GetList(array("DATE_INSERT" => "ASC"), $arFilter);
+while ($ar_sales = $db_sales->Fetch())
+{
+   print_r($db_sales->Fetch());
+}
+?>
